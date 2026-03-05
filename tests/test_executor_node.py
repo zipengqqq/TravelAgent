2c1bc5732514091c5bc1a75fd4b19a0bd032e82b
penn <penn.zi@washiai.com>
2026-03-05 14:47:50 +0800

    优化react实现

--- tests/test_executor_node.py
+++ tests/test_executor_node.py
@@ -19,7 +19,7 @@ async def main():
     state: PlanExecuteState = {
         "question": "北京今天天气怎么样？",
         # "plan": ["从北京市天安门到上海外滩怎么走？"],
-        "plan": ["如何学好langgraph，LangGraph 中最核心的功能是什么"],
+        "plan": ["我在郑州中原区国弘大厦，离我家有多远，我家在中原区东石马小区"],
         "past_steps": [],
         "response": "",
         "route": "planner",
